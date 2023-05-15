3c17c90c3e02427b4d1418252e933cf9b41ad4f1
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-05-15 23:44:32 +0900

    fix: SWEA 2105 디저트 카페

--- swea/jyeon/week12/SWEA_2105_디저트 카페.java
+++ swea/jyeon/week12/SWEA_2105_디저트 카페.java
@@ -1,43 +1,49 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
  
 public class Solution {
  
-    static int TC, N, map[][], ans = -1;
+    static int T, n, map[][], ans = -1;
     static int[] dx = { 1, 1, -1, -1 }, dy = { 1, -1, -1, 1 };
-    static boolean[] isVisited;
+    static boolean[] visited;
  
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
  
-        TC = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= TC; tc++) {
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
             ans = -1;
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            for (int i = 0; i < N; i++) {
+            n = Integer.parseInt(br.readLine());
+            map = new int[n][n];
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < n; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
  
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    isVisited = new boolean[101];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    visited = new boolean[101];
                     dfs(i, j, i, j, 0, 0);
                 }
             }
-            System.out.println("#" + tc + " " + ans);
+            sb.append("#" + t + " " + ans + "\n");
         }
- 
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
  
-    private static void dfs(int x, int y, int initX, int initY, int dir, int cnt) {
-        if (x == initX && y == initY && dir == 3) {
+    private static void dfs(int x, int y, int startX, int startY, int dir, int cnt) {
+        if (x == startX && y == startY && dir == 3) {
             ans = Math.max(ans, cnt);
             return;
         }
@@ -46,13 +52,10 @@ public class Solution {
             if (i <= dir + 1) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-                    continue;
-                if (isVisited[map[nx][ny]])
-                    continue;
-                isVisited[map[nx][ny]] = true;
-                dfs(nx, ny, initX, initY, i, cnt + 1);
-                isVisited[map[nx][ny]] = false;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[map[nx][ny]]) continue;
+                visited[map[nx][ny]] = true;
+                dfs(nx, ny, startX, startY, i, cnt + 1);
+                visited[map[nx][ny]] = false;
             }
         }
     }
